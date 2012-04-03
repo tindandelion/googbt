da706dc2d587e5d7dc5da7758617899468dfc615
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-04-03 11:09:33 +0400

    Implementing the rest of FakeAuctionServer methods

--- test/test/endtoend/auctionsniper/FakeAuctionServer.java
+++ test/test/endtoend/auctionsniper/FakeAuctionServer.java
@@ -1,18 +1,23 @@
 package test.endtoend.auctionsniper;
 
-import org.jivesoftware.smack.Chat;
-import org.jivesoftware.smack.ChatManagerListener;
-import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.XMPPException;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import org.jivesoftware.smack.*;
+import org.jivesoftware.smack.packet.Message;
+
+import java.util.concurrent.ArrayBlockingQueue;
 
 import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class FakeAuctionServer {
     public static final String XMPP_HOSTNAME = "localhost";
     private static final String ITEM_ID_AS_LOGIN = "auction-%s";
     private static final String AUCTION_PASSWORD = "auction";
     private static final String AUCTION_RESOURCE = "Auction";
+
+    private final SingleMessageListener messageListener = new SingleMessageListener();
 
     private String itemId;
     private XMPPConnection connection;
@@ -32,6 +37,7 @@ public class FakeAuctionServer {
                     @Override
                     public void chatCreated(Chat chat, boolean b) {
                         currentChat = chat;
+                        chat.addMessageListener(messageListener);
                     }
                 }
         );
@@ -41,15 +47,27 @@ public class FakeAuctionServer {
         return itemId;
     }
 
-    public void hasReceivedJoinRequestFromSniper() {
-        throw new NotImplementedException();
+    public void hasReceivedJoinRequestFromSniper() throws InterruptedException {
+        messageListener.receivesAMessage();
     }
 
-    public void announceClosed() {
-        throw new NotImplementedException();
+    public void announceClosed() throws XMPPException {
+        currentChat.sendMessage(new Message());
     }
 
     public void stop() {
-        throw new NotImplementedException();
+        connection.disconnect();
+    }
+
+    private class SingleMessageListener implements MessageListener {
+        private final ArrayBlockingQueue<Message> messages = new ArrayBlockingQueue<Message>(1);
+        @Override
+        public void processMessage(Chat chat, Message message) {
+            messages.add(message);
+        }
+
+        public void receivesAMessage() throws InterruptedException {
+            assertThat("Message", messages.poll(5, SECONDS), is(notNullValue()));
+        }
     }
 }
