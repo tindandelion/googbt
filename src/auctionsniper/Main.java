4fac37c01463fa34f707014b57907a741a92face
Sergey Moshnikov <s.moshnikov@gmail.com>
2012-04-03 07:47:36 +0400

    Created AuctionSniperDriver

--- src/auctionsniper/Main.java
+++ src/auctionsniper/Main.java
@@ -1,4 +1,6 @@
 package auctionsniper;
 
 public class Main {
+    public static final String MAIN_WINDOW_FRAME = "";
+    public static final String SNIPER_STATUS_NAME = "";
 }
--- test/test/endtoend/auctionsniper/AuctionSniperDriver.java
+++ test/test/endtoend/auctionsniper/AuctionSniperDriver.java
@@ -1,4 +1,24 @@
 package test.endtoend.auctionsniper;
 
-public class AuctionSniperDriver {
+import auctionsniper.Main;
+import com.objogate.wl.swing.AWTEventQueueProber;
+import com.objogate.wl.swing.driver.JFrameDriver;
+import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.gesture.GesturePerformer;
+
+import static org.hamcrest.Matchers.equalTo;
+
+@SuppressWarnings("ALL")
+public class AuctionSniperDriver extends JFrameDriver {
+    public AuctionSniperDriver(int timeoutMillis) {
+        super(new GesturePerformer(),
+                JFrameDriver.topLevelFrame(
+                        named(Main.MAIN_WINDOW_FRAME),
+                        showingOnScreen()),
+                new AWTEventQueueProber(timeoutMillis, 100));
+    }
+
+    public void showsSniperStatus(String statusText) {
+        new JLabelDriver(this, named(Main.SNIPER_STATUS_NAME)).hasText(equalTo(statusText));
+    }
 }
